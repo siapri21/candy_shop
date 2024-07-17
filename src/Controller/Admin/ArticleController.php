bf6b069932c968da0fdbfa7ab467e470a4a9d7ac
siapri21 <siapriouattara21@gmail.com>
2024-07-17 14:03:38 +0200

    insert into

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Candy;
+use App\Repository\CandyRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 #[Route('/admin/article', 'admin_article_')]
 class ArticleController extends AbstractController
@@ -16,8 +21,37 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/create', name: 'create')]
-    public function create():Response
+    public function create(EntityManagerInterface $em ):Response
     {
+        $candy= new Candy();
+        $candy->setName('bonbon au lait')
+        ->setSlog('fraise')
+        ->setDescription('super bonbon')
+        ->setCreateAt(new DateTimeImmutable());
+
+// $em->persist($candy); : La méthode persist prend un objet en argument (dans ce cas, $candy) et le marque comme "persistant" dans l'EntityManager. Cela signifie que l'objet est ajouté à la liste des objets à sauvegarder dans la base de données lors de la prochaine opération de flush.
+
+// $em->persist($candy); : La méthode persist prend un objet en argument (dans ce cas, $candy) et le marque comme "persistant" dans l'EntityManager. Cela signifie que l'objet est ajouté à la liste des objets à sauvegarder dans la base de données lors de la prochaine opération de flush.
+        $em->persist($candy);
+
+        // flush dit à Doctrine : "Ok, j'ai fini de préparer les objets à sauvegarder. Maintenant, écris-les dans la base de données."
+        $em->flush();
+
+        dd($candy);
+
         return $this->render('admin/article/create.html.twig');
+    }
+
+     #[Route('/update/{id}', name: 'update', requirements: ['id' => Requirement::DIGITS])]
+    public function update($id, CandyRepository $repository):Response
+    {
+        dd($repository);
+        return $this->render('admin/article/update.html.twig');
+    }
+
+    #[Route('/delete/{id}', name: 'delete')]
+    public function delete():Response
+    {
+        return $this->render('admin/article/delete.html.twig');
     }
 }
