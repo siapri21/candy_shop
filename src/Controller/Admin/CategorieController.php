a4b8fa626902a99fd39677731a41f130aa39b12f
siapri21 <siapriouattara21@gmail.com>
2024-07-18 12:32:53 +0200

    form uspdate

--- src/Controller/Admin/CategorieController.php
+++ src/Controller/Admin/CategorieController.php
@@ -3,13 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Categorie;
-use App\Repository\CategorieRepository;
+use App\Form\CategorieType;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Routing\Requirement\Requirement;
+
 
 #[Route('/admin/categorie', name: 'admin_categorie_')]
 class CategorieController extends AbstractController
@@ -44,18 +45,28 @@ class CategorieController extends AbstractController
         return $this->render('admin/categorie/create.html.twig');
     }
 
-    #[Route('/update{id}', name: 'update')]
-    public function update($id, Categorie $categorie): Response
+    #[Route('/update/{id}', name: 'update')]
+    public function update($id, Categorie $categorie, Request $request): Response
     {
-       
-        $categorie->setName('sucette')
-    ->setDescription('super')
-    ->setCreateAt(new DateTimeImmutable());
-        $this-> em->flush();
-        return $this->render('admin/categorie/update.html.twig');
+        $form = $this->createForm(CategorieType::class , $categorie);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $this->em->flush();
+            $this->redirectToRoute('admin_categorie_index');
+        }
+    //      $categorie;
+    //     $categorie->setName('sucette')
+    // ->setDescription('super')
+    // ->setCreateAt(new DateTimeImmutable());
+    //     $this-> em->flush();
+        return $this->render('admin/categorie/update.html.twig',
+    ['form' =>$form]);
     }
 
-    #[Route('/delete{id}', name: 'delete')]
+    #[Route('/delete/{id}', name: 'delete')]
     public function delete(Categorie $categorie): Response
     {
 
--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategorieType extends AbstractType
@@ -12,7 +15,7 @@ class CategorieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class)
             ->add('description')
             ->add('createAt', null, [
                 'widget' => 'single_text',
@@ -20,6 +23,7 @@ class CategorieType extends AbstractType
             ->add('updateAt', null, [
                 'widget' => 'single_text',
             ])
+            
         ;
     }
 
